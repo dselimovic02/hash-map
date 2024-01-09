7f0f909067c60dcbb89e52af9086c3deb5d31583
dselimovic <dselimovic02@gmail.com>
2024-01-09 15:05:22 +0100

    Set up HashMap class

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -1,11 +1,18 @@
 class HashMap
 
+
+  def initialize
+    @buckets = Array.new(16)
+    @capacity = 0
+    @load_factor = 0.75 
+  end
+
   def hash(string)
     hash_num = 0
     prime_num = 31
 
-    string.each{ |char| prime_num * hash_num + char.ord}
+    string.each_char{ |char| hash_num = prime_num * hash_num + char.ord}
 
-    hash_num
+    hash_num % @buckets.length
   end
 end
---
+++ main.rb
@@ -0,0 +1,6 @@
+require_relative 'lib/hash_map'
+
+hash_map = HashMap.new
+
+p Array.new(16)
+p hash_map.hash('ednis')
